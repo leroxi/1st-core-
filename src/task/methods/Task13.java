3bfb4bcbff96ee3339bf2360a11376afa67cfe86
Ленар <160050788+leroxi@users.noreply.github.com>
2024-04-27 00:14:05 +0300

    Update Task13.java

--- src/task/methods/Task13.java
+++ src/task/methods/Task13.java
@@ -1,20 +1,2 @@
-package task.methods;
-
-public class Task13 {
-    public static void main(String[] args) {
-        getCountedLetters();
-    }
-
-    public static void getCountedLetters() {
-        String str = "AAA";
-        int index = 0;
-        for (char elem : str.toCharArray()) {
-            if (elem == 'A') index++;
-        }
-        System.out.println(index + " Букв А в строке ");
-
-
-    }
-}
 
 
