34e7b3347f35b8927064999a49dd34157cc1666f
Ленар <160050788+leroxi@users.noreply.github.com>
2024-04-28 23:23:05 +0300

    Add files via upload

---
+++ src/task/oop/Employee.java
@@ -0,0 +1,52 @@
+public abstract class Employee {
+    private String name;
+    private int age;
+    private char accessLevel;
+    private int id;
+
+    public Employee(String name, int age, int id, char accessLevel) {
+        this.name = name;
+        this.age = age;
+        this.id = (int) (Math.random() * 6);
+        this.accessLevel = accessLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            System.out.println("Не корректное имя ");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int vozrast) {
+        if (vozrast < 18) {
+            System.out.println("Не корректный возраст ");
+        } else if (vozrast > 100) {
+            System.out.println("Не корректный возраст ");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public char getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevelchar() {
+        this.accessLevel = accessLevel;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
---
+++ src/task/oop/WarehouseWorker.java
@@ -0,0 +1,12 @@
+public class WarehouseWorker extends Employee {
+
+    private int fingers;
+
+    public WarehouseWorker(String name, int age, int id, char accessLevel, int fingers) {
+        super(name, age, id,  accessLevel);
+        this.fingers = fingers;
+    }
+
+    WarehouseWorker Alexa = new WarehouseWorker("Vasik", 15, 10,  'D', 19 );
+
+}
